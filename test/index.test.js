011eeb361bd2eb90ece253eb19cd7ec7ee0afd2e
liyangtao <liyangtao@meituan.com>
2018-05-25 19:23:53 +0800

    fix ignore

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const shell = require('shelljs');
-const assert =  require('assert');
-describe('denpendency tool', () => {
-  it('generate', () => {
-    console.log('ge')
-    assert.equal(12,12);
-  });
-});
